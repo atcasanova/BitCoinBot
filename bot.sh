e30e8f36e29f21c6c3f61376a345668f6d1a64a9
Alfredo Casanova <atcasanova@gmail.com>
2018-05-06 10:42:55 -0300

    Consulta de bankroll agora mostra grafico de distribuicao de moedas com google image chart api.

--- bot.sh
+++ bot.sh
@@ -281,7 +281,7 @@ BTC ${totalbtc}\`\`\`"
 	argmoeda=${argmoeda::-1}
 	cores=$(cat $dono.coins | wc -l)
 	
-	wget "https://chart.googleapis.com/chart?cht=p3&chd=t:$argvalor&chs=600x500&chdl=$argmoeda&chco=$(echo ${COLORS[@]:0:$cores} |tr ' ' '|')&chds=a" -Ograph.png
+	wget "https://chart.googleapis.com/chart?cht=p3&chd=t:$argvalor&chs=600x500&chdl=$argmoeda&chco=$(echo ${COLORS[@]:0:$cores} |tr ' ' '|')&chds=a&chtt=$dono" -Ograph.png
 	grafico=$(curl -s -X POST "$apiurl/sendPhoto" -F chat_id=$CHATID -F photo=@graph.png |\
         jq -r '.result.photo[] | .file_id' | tail -1)
 }
