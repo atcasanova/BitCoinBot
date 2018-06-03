0b3b3dd23dc549a9e94b8b7ae7adbbfc9b6c21e7
Alfredo Casanova <atcasanova@gmail.com>
2018-06-03 13:05:57 -0300

    grid lines

--- bot.sh
+++ bot.sh
@@ -297,7 +297,7 @@ evolucao(){
 		arg+=$valor,
 	done < $dono.history
 	arg=${arg::-1}
-	wget -q "https://chart.googleapis.com/chart?cht=lc&chd=t:$arg&chs=600x500&chtt=Evolução%20do%20Stack%20de%20$dono&chxt=y&chds=a" -Oout.png
+	wget -q "https://chart.googleapis.com/chart?cht=lc&chd=t:$arg&chs=600x500&chtt=Evolução%20do%20Stack%20de%20$dono&chxt=y&chds=a&chg=10,10" -Oout.png
 	grafico=$(curl -s -X POST "$apiurl/sendPhoto" -F chat_id=$CHATID -F photo=@out.png |\
 	jq -r '.result.photo[] | .file_id' | tail -1)
 	rm out.png
