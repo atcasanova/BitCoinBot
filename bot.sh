3965254251471960c5c1c39646276d98689f019e
Alfredo Casanova <atcasanova@gmail.com>
2018-11-07 23:44:01 -0200

    extra parenthesis

--- bot.sh
+++ bot.sh
@@ -295,15 +295,19 @@ evolucao(){
 	[ ! -s $dono.history ] && {
 		envia "Nao ha dados para $dono. Use /consulta antes";
 	} || {
-		arg=
-		while IFS=, read data valor; do
-			arg+=$valor,
-		done < $dono.history
-		arg=${arg::-1}
-		wget -q "https://chart.googleapis.com/chart?cht=lc&chd=t:$arg&chs=600x500&chtt=Evolução%20do%20Stack%20de%20$dono&chxt=y&chds=a&chg=10,10" -Oout.png
-		grafico=$(curl -s -X POST "$apiurl/sendPhoto" -F chat_id=$CHATID -F photo=@out.png |\
-		jq -r '.result.photo[] | .file_id' | tail -1)
-		rm out.png
+		(( $(cat $dono.history | wc -l ) < 2 )) && {
+			envia "Poucos registros para gerar o grafico para @$dono. Mínimo: 2";
+		} || {
+			arg=
+			while IFS=, read data valor; do
+				arg+=$valor,
+			done < $dono.history
+			arg=${arg::-1}
+			wget -q "https://chart.googleapis.com/chart?cht=lc&chd=t:$arg&chs=600x500&chtt=Evolução%20do%20Stack%20de%20$dono&chxt=y&chds=a&chg=10,10" -Oout.png
+			grafico=$(curl -s -X POST "$apiurl/sendPhoto" -F chat_id=$CHATID -F photo=@out.png |\
+			jq -r '.result.photo[] | .file_id' | tail -1)
+			rm out.png
+		}
 	}
 }
 
